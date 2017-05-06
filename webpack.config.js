313fb825d61e9b6d2bb757020cb38381a826775c
Thomas Boerger <thomas@webhippie.de>
2017-05-06 20:16:18 +0200

    Manage assets via webpack and npm

---
+++ webpack.config.js
@@ -0,0 +1,105 @@
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+var Webpack = require('webpack');
+
+module.exports = {
+	entry: {
+		app: [
+			'./assets/index.js'
+		]
+	},
+
+	output: {
+		path: require('path').resolve(__dirname, 'static'),
+		filename: 'scripts/docs.js',
+		publicPath: '/'
+	},
+
+	devtool: 'source-map',
+
+	resolve: {
+		extensions: [
+			'',
+			'.js'
+		]
+	},
+
+	module: {
+		loaders: [
+			{
+				test: /\.js?$/,
+				exclude: /node_modules/,
+				loader: 'babel',
+				query: {
+					presets: [
+						'es2015'
+					]
+				}
+			},
+			{
+				test: /\.less$/,
+				loader: ExtractTextPlugin.extract(
+					'css?sourceMap!less?sourceMap'
+				)
+			},
+			{
+				test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+				loader: 'file?name=fonts/[name].[ext]&mimetype=application/font-woff'
+			},
+			{
+				test: /\.otf(\?v=\d+\.\d+\.\d+)?$/,
+				loader: 'file?name=fonts/[name].[ext]&mimetype=application/x-font-opentype'
+			},
+			{
+				test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+				loader: 'file?name=fonts/[name].[ext]&mimetype=image/svg+xml'
+			},
+			{
+				test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+				loader: 'file?name=fonts/[name].[ext]&mimetype=application/octet-stream'
+			},
+			{
+				test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+				loader: 'file?name=fonts/[name].[ext]&mimetype=application/vnd.ms-fontobject'
+			}
+		]
+	},
+
+	vue: {
+		loaders: {
+			css: ExtractTextPlugin.extract("css!less"),
+		}
+	},
+
+	plugins: [
+		new ExtractTextPlugin(
+			'styles/docs.css'
+		),
+		new CopyWebpackPlugin([{
+			from: 'assets/images',
+			to: 'images'
+		}]),
+		new Webpack.DefinePlugin({
+			'process.env.NODE_ENV': JSON.stringify(
+				process.env.NODE_ENV || 'development'
+			)
+		}),
+		new Webpack.optimize.OccurenceOrderPlugin(),
+		new Webpack.optimize.DedupePlugin()
+	]
+};
+
+if (process.env.NODE_ENV === 'production') {
+	module.exports.devtool = '#source-map';
+
+	module.exports.plugins = (module.exports.plugins || []).concat([
+		new Webpack.optimize.UglifyJsPlugin({
+			compress: {
+				warnings: false
+			},
+			output: {
+				semicolons: false
+			}
+		})
+	]);
+}
